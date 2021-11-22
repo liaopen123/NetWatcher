e451912527a118c5db0a1b9b44e44fb839f3de14
liaopenghui <liaopenghui@haohaozhu.com>
2021-11-22 18:14:12 +0800

    新增api

--- hhz_lib_net_reporter/src/main/java/com/hzhu/networkrequestreport/utils/BlackListUtils.kt
+++ hhz_lib_net_reporter/src/main/java/com/hzhu/networkrequestreport/utils/BlackListUtils.kt
@@ -3,16 +3,28 @@ package com.hzhu.networkrequestreport.utils
 import java.util.*
 
 object BlackListUtils {
-    private val urlBlackList = arrayOf<String>("resshow")
+    private val urlBlackList = ArrayList<String>()
+
+    init {
+        urlBlackList.add("resshow")
+    }
 
     fun contain(url: String): Boolean {
         for (keywords in urlBlackList) {
-            if (url.toLowerCase(Locale.ROOT).contains(keywords)){
+            if (url.toLowerCase(Locale.ROOT).contains(keywords)) {
                 return true
             }
 
         }
         return false
+    }
+
+    fun add(keywords: String) {
+        urlBlackList.add(keywords)
+    }
+
+    fun remove(keywords: String) {
+        urlBlackList.remove(keywords)
     }
 
 }
